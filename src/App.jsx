8345599dafbc1807cbe9472be68ab840d40e2f67
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2024-09-25 22:47:00 +0530

    Initialize project with shadcn and react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+	return (
+	  <div>
+
+			<div>App</div>
+	  </div>
+  )
+}
+
+export default App
